4a75f4fd44cce1d3989988bdef5d28fc038a2ab2
happysmilecode <kevinharuto430@gmail.com>
2024-09-21 23:26:21 -0700

    first commit

---
+++ src/components/ramps/RampsCard.jsx
@@ -0,0 +1,38 @@
+import styles from "./RampsCard.module.scss";
+import RampIconPlaceholder from "./assets/ramp-icon-placeholder.png";
+import Image from "next/image";
+import { useTranslation } from "next-i18next";
+
+const RampsCard = ({
+  rampIndex,
+  title,
+  description,
+  showModalOnClick,
+  imageUrl,
+}) => {
+  const { t } = useTranslation();
+
+  return (
+    <button
+      data-index={rampIndex}
+      className={`${styles["ramps-card"]}`}
+      onClick={showModalOnClick}
+    >
+      <div className={`${styles["ramps-card__icon-container"]}`}>
+        <Image
+          src={imageUrl ?? RampIconPlaceholder}
+          width={61}
+          height={61}
+          alt=""
+        />
+      </div>
+      <h2 className={`${styles["ramps-card__title"]}`}>{title}</h2>
+      <p className={`${styles["ramps-card__description"]}`}>{description}</p>
+      <span className={`${styles["ramps-card__details"]}`}>
+        {t("on-off-ramp.cards.view-details-title")} <span>+</span>
+      </span>
+    </button>
+  );
+};
+
+export default RampsCard;
